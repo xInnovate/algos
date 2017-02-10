d20326295d10f291c3ed36cc7222e553f7bc6901
xInnovate <shakashby@gmail.com>
2017-02-10 12:41:50 -0400

    Implemented linearSearch function and compiled data from the internet

--- search.cpp
+++ search.cpp
@@ -67,3 +67,12 @@ int main()
 
     return 0;
 }
+
+int linearSearch (auto data, auto key) {
+	for (int i = 0; i < data.size(); i++) {
+		if (data[i] == key) {
+			return i;
+		}
+	}
+	return -1;
+}
